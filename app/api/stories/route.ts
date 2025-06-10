66fb40530d233998774eaedddcb4cdb95870dc78
Polina Balandina <polyabalandina2004@gmail.com>
2025-06-10 20:48:11 +0300

    apdate

--- app/api/stories/route.ts
+++ app/api/stories/route.ts
@@ -2,11 +2,15 @@ import { prisma } from '@/prisma/prisma-client';
 import { NextResponse } from 'next/server';
 
 export async function GET() {
-  const stories = await prisma.story.findMany({
-    include: {
-      items: true,
-    },
-  });
-
-  return NextResponse.json(stories);
+  try {
+    const stories = await prisma.story.findMany({
+      include: {
+        items: true,
+      },
+    });
+    return NextResponse.json(stories);
+  } catch (error) {
+    console.error('Failed to fetch stories:', error);
+    return new NextResponse('Server Error', { status: 500 });
+  }
 }
